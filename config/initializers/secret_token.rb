9e809d7003748f2da3561e52806fa3fa8aa8eb67
Samuel McTaggart <sam.mctaggart@gmail.com>
2013-06-11 15:09:21 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExtremeMovies::Application.config.secret_token = 'f17afa669d67cb76aa92c15b826c57c3b5a211701273ac3e22d5842dd756aa12264013582df869ff1466570c0d6f38ac59f5cdc8b0f558d90dafbee535fa1b85'

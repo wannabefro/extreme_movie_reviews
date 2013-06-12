57a5fcbe0e6ef018c4563038296eb1e00341c913
Christopher Lemieux <your_email@whatever.com>
2013-06-12 16:10:58 -0400

    Created model finished user test

---
+++ app/models/movie.rb
@@ -0,0 +1,6 @@
+class Movie < ActiveRecord::Base
+  attr_accessible :title, :year
+  validates_uniqueness_of :title
+  validates_presence_of :title, :year
+  validates_length_of :year, :is => 4, :message => 'Must be four digits'
+end

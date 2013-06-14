e3fa5a8800fac4e6a97a1c91aa81a898c17d435b
Christopher Lemieux <your_email@whatever.com>
2013-06-13 22:09:04 -0400

    validations for year fixed

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,7 +4,7 @@ class Movie < ActiveRecord::Base
   attr_accessible :title, :year
   validates_uniqueness_of :title, :scope => :year, :message => 'Movie already created'
   validates_presence_of :title, :year, :message => "Invalid Criteria"
-  validates :year, :inclusion => 1875...(Time.now.year)
+  validates :year, :inclusion => 1875..(Time.now.year + 1)
 
 
 

e9602ad017e1865b20ae26e663151cc516573f3a
Christopher Lemieux <your_email@whatever.com>
2013-06-13 22:01:55 -0400

    validation for year changed

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,5 +4,12 @@ class Movie < ActiveRecord::Base
   attr_accessible :title, :year
   validates_uniqueness_of :title, :scope => :year, :message => 'Movie already created'
   validates_presence_of :title, :year, :message => "Invalid Criteria"
-  validates_length_of :year, :is => 4, :message => 'Must be four digits'
+  validates :year, :inclusion => 1875...(Time.now.year)
+
+
+
 end
+
+
+
+

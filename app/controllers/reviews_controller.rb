49be1d1d065d03d5f178eee1a727d5cae8d36153
Jason Zopf <jasonzopf@gmail.com>
2013-06-18 15:53:32 -0400

    fix controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,11 +5,9 @@ class ReviewsController < ApplicationController
   def create
     @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.build(params[:review])
-    @review_all = Review.all
     @review.user = current_user
 
-   
-    if @review.save(params[:review])
+    if @review.save
       redirect_to movie_path(@movie)
     else
       flash[:notice] = 'Cannot save review. You cannot review the same movie twice, and fields cannot be empty.'
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,7 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :recent_reviews
 
-  attr_accessible :title, :body, :movie_id, :user_id, :user, :movie
+  attr_accessible :title, :body, :user, :movie
 
   validates_presence_of :title, :body, message: "Cannot be blank"
   validates_uniqueness_of :user_id, scoped_to: :movie_id
